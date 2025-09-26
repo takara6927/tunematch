a8c63664fa760ee8952fca2741a59d657b0d5608
takara6927 <takara6927@gmail.com>
2025-09-26 18:23:27 +0900

    Add safe_avatar_tag helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def safe_avatar_tag(user, size: [128,128], **opts)
+    if user.image.attached?
+      begin
+        image_tag user.image.variant(resize_to_fill: size).processed, **opts
+      rescue ActiveStorage::FileNotFoundError
+        user.image.purge_later
+        content_tag(:div, (user.name.to_s.first&.upcase || "U"), class: "u-show_avatar-fallback")
+      end
+    else
+      content_tag(:div, (user.name.to_s.first&.upcase || "U"), class: "u-show_avatar-fallback")
+    end
+  end
 end
